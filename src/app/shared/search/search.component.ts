3f3f2d2e352063aa7a6f03404c88be4db98bff4b
Diegodossantos23 <diegodossantos900@outlook.com>
2024-07-23 12:34:34 -0300

    fix: change from search input to status, species and gender selects

--- src/app/shared/search/search.component.ts
+++ src/app/shared/search/search.component.ts
@@ -1,6 +1,32 @@
 import { Component, Output, EventEmitter } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+enum Status {
+  Alive = 'alive',
+  Dead = 'dead',
+  Unknown = 'unknown'
+}
+
+enum Gender {
+  Female = 'female',
+  Male = 'male',
+  Genderless = 'genderless',
+  Unknown = 'unknown'
+}
+
+enum Species {
+  Alien = 'Alien',
+  Animal = 'Animal',
+  Cronenberg = 'Cronenberg',
+  Disease = 'Disease',
+  MythologicalCreature = 'Mythological Creature',
+  Human = 'Human',
+  Humanoid = 'Humanoid',
+  Poopybutthole = 'Poopybutthole',
+  Robot = 'Robot',
+  Unknown = 'unknown'
+}
+
 interface SearchFilters {
   [key: string]: string;
 }
@@ -15,11 +41,15 @@ interface SearchFilters {
 export class SearchComponent {
   filters: SearchFilters = {};
 
+  statuses = Object.values(Status);
+  speciesList = Object.values(Species);
+  genders = Object.values(Gender);
+
   @Output() searchEvent = new EventEmitter<SearchFilters>();
 
   onSearch(event: Event, filterType: string): void {
-    const inputElement = event.target as HTMLInputElement;
-    const query = inputElement.value;
+    const target = event.target as HTMLInputElement | HTMLSelectElement;
+    const query = target.value;
 
     if (query) {
       this.filters[filterType] = query;
